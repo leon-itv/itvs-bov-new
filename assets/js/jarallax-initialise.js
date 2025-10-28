df72f3a29315bfd35bc3c7951a904d58fd730a4e
Leon L <92985165+leon-itv@users.noreply.github.com>
2025-10-28 10:37:49 +0000

    Project Files Upload

---
+++ assets/js/jarallax-initialise.js
@@ -0,0 +1,10 @@
+// external js: jarallax.js
+
+jarallax(document.querySelectorAll('.jarallax'), {
+    disableParallax: function () {
+        return /iPad|iPhone|iPod|Android/.test(navigator.userAgent);
+    },
+    disableVideo: function () {
+        return /iPad|iPhone|iPod|Android/.test(navigator.userAgent);
+    }
+});
---
+++ assets/js/matchHeight-initialise.js
@@ -0,0 +1,8 @@
+// external js: matchHeight-initialise.js
+
+window.onload = function () {
+$('.mh1').matchHeight();
+$('.mh2').matchHeight();
+$('.mh3').matchHeight();
+$(".loader").fadeOut("slow");
+};
